02b00442ca76e212b23dd133b969a2c57670f524
tomas.kravcik <thomas.kravcik@gmail.com>
2026-03-02 21:50:05 +0100

    ci: Add GitHub Actions workflow for static Astro site deployment to GitHub Pages and configure Astro output to static.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tomasastro.cz',
-  output: 'server',
+  output: 'static',
 
   i18n: {
     defaultLocale: 'cz',
